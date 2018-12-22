7764c1fc5a3f33f9b2ddc00e85f99737131eb1a3
Randy Olive <randy.jr.olive@gmail.com>
2018-12-22 17:07:22 -0500

    mod dockerignore and added collectstatic to start script

--- start-dev.sh
+++ start-dev.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "Starting CanGrow Deployment"
-# python3 manage.py migrate
+python3 manage.py collectstatic --noinput
 celery multi start 1 -A cangrow -B -n cangrow --logfile=/code/log/celery/cangrow_worker.log --pidfile=/celery-pidfiles/%n.pid
 gunicorn cangrow.wsgi -b 0.0.0.0:8000 --timeout 300 >> /code/log/cangrow/cangrow.log
