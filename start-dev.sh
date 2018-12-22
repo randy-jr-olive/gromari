a0ef8544f7ce2046cf67ab8bc6c20f7fc7bb1ba2
Randy Olive <randy.jr.olive@gmail.com>
2018-12-22 18:54:14 -0500

    updated start script and changed logo

--- start-dev.sh
+++ start-dev.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 echo "Starting CanGrow Deployment"
+echo "Running migrations:"
+python3 manage.py makemigrations
+python3 manage.py migrate
+echo "Done migrations"
+echo "Running collect static:"
 python3 manage.py collectstatic --noinput
+echo "done collectstatic"
+echo "Starting celery"
 celery multi start 1 -A cangrow -B -n cangrow --logfile=/code/log/celery/cangrow_worker.log --pidfile=/celery-pidfiles/%n.pid
+echo "Starting CanGrow server"
 gunicorn cangrow.wsgi -b 0.0.0.0:8000 --timeout 300 >> /code/log/cangrow/cangrow.log
