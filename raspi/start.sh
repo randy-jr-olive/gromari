13c18a2775331922fca6c2ab912daebdc90ec5c2
Randy Olive <randy.jr.olive@gmail.com>
2019-01-05 13:04:42 -0500

    updated start script raspi

--- raspi/start.sh
+++ raspi/start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "Starting gromari api server"
-ls -la
+RUN mkdir -p ./log/api
+RUN touch ./log/api/api.log
 python3 coretemp/coretemp.py &
 docker-compose -f docker-compose.yml up --force-recreate
