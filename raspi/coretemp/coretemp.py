a17f4f59f0879cb86305afafd97e6b8d714f053e
Randy Olive <randy.jr.olive@gmail.com>
2019-01-05 13:29:10 -0500

    removed thread for docker-compose, changed print from coretemp script

--- raspi/coretemp/coretemp.py
+++ raspi/coretemp/coretemp.py
@@ -4,6 +4,4 @@ def measure_temp():
         temp = os.popen("vcgencmd measure_temp").readline()
         return (temp.replace("temp=",""))
 
-print("getting temp:")
-print(measure_temp())
 print(measure_temp(), file=open("./coretemp/coretemp.value", "w"))
