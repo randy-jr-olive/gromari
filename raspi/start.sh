4a970ff08964b55a37f684ee3efd21fcf84a2b04
Randy Olive <randy.jr.olive@gmail.com>
2019-01-05 13:35:56 -0500

    redirect watch output to null

--- raspi/start.sh
+++ raspi/start.sh
@@ -3,5 +3,5 @@
 echo "Starting gromari api server"
 mkdir -p ./log/api
 touch ./log/api/api.log
-watch -n 5 python3 coretemp/coretemp.py &
+watch -n 5 python3 coretemp/coretemp.py >> /dev/null &
 docker-compose -f docker-compose.yml up --force-recreate
