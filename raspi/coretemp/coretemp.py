d505793cf8bcf835bc0178412f662c066716d36d
Randy Olive <randy.jr.olive@gmail.com>
2019-01-05 13:22:11 -0500

    changed to watch for interval control start script

--- raspi/coretemp/coretemp.py
+++ raspi/coretemp/coretemp.py
@@ -1,12 +1,9 @@
 import os
-import time
 
 def measure_temp():
         temp = os.popen("vcgencmd measure_temp").readline()
         return (temp.replace("temp=",""))
 
-while True:
-        print("getting temp:")
-        print(measure_temp())
-        print(measure_temp(), file=open("./coretemp/coretemp.value", "w"))
-time.sleep(5)
+print("getting temp:")
+print(measure_temp())
+print(measure_temp(), file=open("./coretemp/coretemp.value", "w"))
