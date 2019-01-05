efa467f50ee18767968cdcbdb628e7ff47542637
Randy Olive <randy.jr.olive@gmail.com>
2019-01-05 13:05:25 -0500

    removed RUN commands from start script

--- raspi/start.sh
+++ raspi/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Starting gromari api server"
-RUN mkdir -p ./log/api
-RUN touch ./log/api/api.log
+mkdir -p ./log/api
+touch ./log/api/api.log
 python3 coretemp/coretemp.py &
 docker-compose -f docker-compose.yml up --force-recreate
