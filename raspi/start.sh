a0d54e847dacd70e1a6a352ea6af2141e35b0d9b
Randy Olive <randy.jr.olive@gmail.com>
2019-01-05 13:26:42 -0500

    changed watch time to 5 seconds

--- raspi/start.sh
+++ raspi/start.sh
@@ -3,5 +3,5 @@
 echo "Starting gromari api server"
 mkdir -p ./log/api
 touch ./log/api/api.log
-watch -n 60 python3 coretemp/coretemp.py &
+watch -n 5 python3 coretemp/coretemp.py &
 docker-compose -f docker-compose.yml up --force-recreate &
