c44f871444c234e096f283f1401c7151a839b397
Randy Olive <randy.jr.olive@gmail.com>
2019-01-08 17:22:46 -0500

    added del stage

--- raspi/start-enviro.sh
+++ raspi/start-enviro.sh
@@ -1,1 +1,5 @@
-watch -n 5 sudo /home/pi/sources/Adafruit_Python_DHT/examples/AdafruitDHT.py 2302 4 | tee /home/pi/gromari/raspi/enviro/enviro.value
+while true
+do
+    sudo rm -rf /home/pi/gromari/raspi/enviro/enviro.value
+    sudo /home/pi/sources/Adafruit_Python_DHT/examples/AdafruitDHT.py 2302 4 >> /home/pi/gromari/raspi/enviro/enviro.value
+done
