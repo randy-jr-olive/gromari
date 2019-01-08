5c2c079a4442f60453032f32af0ef9ddd6a2348a
Randy Olive <randy.jr.olive@gmail.com>
2019-01-08 17:02:01 -0500

    correct sudo permissions issues

--- raspi/start-enviro.sh
+++ raspi/start-enviro.sh
@@ -1,4 +1,4 @@
 while true
 do
-    sudo /home/pi/sources/Adafruit_Python_DHT/examples/AdafruitDHT.py 2302 4 | tee enviro/enviro.value
+    sudo /home/pi/sources/Adafruit_Python_DHT/examples/AdafruitDHT.py 2302 4 | sudo tee /home/pi/gromari/raspi/enviro/enviro.value
 done
