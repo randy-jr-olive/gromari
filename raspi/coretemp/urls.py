6791e3d48b12bbc832206d6dbde5fcdd5e93999e
Randy Olive <randy.jr.olive@gmail.com>
2019-01-08 17:45:05 -0500

    fix for views import error

--- raspi/coretemp/urls.py
+++ raspi/coretemp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from . import views
+from coretemp import views
 
 urlpatterns = [
     path('', views.coretemp, name='coretemp'),
--- raspi/enviro/urls.py
+++ raspi/enviro/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from . import views
+from enviro import views
 
 urlpatterns = [
     path('', views.enviro, name='enviro'),
