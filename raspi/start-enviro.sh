a7b716d9888a900c114f6d9f993f34f7a942ea84
Randy Olive <randy.jr.olive@gmail.com>
2019-01-08 16:51:27 -0500

    redirect script output

--- raspi/start-enviro.sh
+++ raspi/start-enviro.sh
@@ -1,4 +1,4 @@
 while true
 do
-    sudo /home/pi/sources/Adafruit_Python_DHT/examples/AdafruitDHT.py 2302 4 2>&1 | tee enviro/enviro.value
+    sudo /home/pi/sources/Adafruit_Python_DHT/examples/AdafruitDHT.py 2302 4 >> enviro/enviro.value
 done
