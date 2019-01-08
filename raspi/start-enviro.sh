c271ba119981aa33833f38d5c98a1085d590ba36
Randy Olive <randy.jr.olive@gmail.com>
2019-01-08 17:08:52 -0500

    tee

--- raspi/start-enviro.sh
+++ raspi/start-enviro.sh
@@ -1,1 +1,1 @@
-watch -n 5 sudo /home/pi/sources/Adafruit_Python_DHT/examples/AdafruitDHT.py 2302 4 > /home/pi/gromari/raspi/enviro/enviro.value
+watch -n 5 sudo /home/pi/sources/Adafruit_Python_DHT/examples/AdafruitDHT.py 2302 4 | tee /home/pi/gromari/raspi/enviro/enviro.value
