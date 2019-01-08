e4d050550f1872327a4318fd9652f31f81c358e5
Randy Olive <randy.jr.olive@gmail.com>
2019-01-08 17:07:55 -0500

    script

--- raspi/start-enviro.sh
+++ raspi/start-enviro.sh
@@ -1,4 +1,1 @@
-while true
-do
-    sudo /home/pi/sources/Adafruit_Python_DHT/examples/AdafruitDHT.py 2302 4 > /home/pi/gromari/raspi/enviro/enviro.value
-done
+watch -n 5 sudo /home/pi/sources/Adafruit_Python_DHT/examples/AdafruitDHT.py 2302 4 > /home/pi/gromari/raspi/enviro/enviro.value
