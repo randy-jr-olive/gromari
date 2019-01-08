6f044dd7b3d2cf4efe32e9f78160bc785e07e17d
Randy Olive <randy.jr.olive@gmail.com>
2019-01-08 18:25:22 -0500

    fix start script

--- raspi/start.sh
+++ raspi/start.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 echo "Starting gromari api server"
-mkdir -p ./log/api
-touch ./log/api/api.log
-watch -n 5 python3 coretemp/coretemp.py >> /dev/null &
+mkdir -p /home/pi/gromari/raspi/log/api
+touch /home/pi/gromari/raspi/log/api/api.log
+watch -n 5 python3 /home/pi/gromari/raspi/coretemp/coretemp.py >> /dev/null &
 watch -n 5 sudo /home/pi/sources/Adafruit_Python_DHT/examples/AdafruitDHT.py 2302 4 >> /dev/null &
+cd /home/pi/gromari/raspi/
 docker-compose -f docker-compose.yml up --force-recreate
