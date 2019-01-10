b394f71873e7310663e192fd64baa6fcd0755e22
Randy Olive <randy.jr.olive@gmail.com>
2019-01-10 18:20:24 -0500

    fix for start script

--- raspi/coretemp/coretemp.py
+++ raspi/coretemp/coretemp.py
@@ -4,4 +4,4 @@ def measure_temp():
         temp = os.popen("vcgencmd measure_temp").readline()
         return (temp.replace("temp=",""))
 
-print(measure_temp(), file=open("./coretemp/coretemp.value", "w"))
+print(measure_temp(), file=open("/home/pi/gromari/raspi/coretemp/coretemp.value", "w"))
--- raspi/start.sh
+++ raspi/start.sh
@@ -3,7 +3,7 @@
 echo "Starting gromari api server"
 mkdir -p /home/pi/gromari/raspi/log/api
 touch /home/pi/gromari/raspi/log/api/api.log
-watch -n 5 python3 /home/pi/gromari/raspi/coretemp/coretemp.py >> /dev/null &
-watch -n 5 sudo /home/pi/sources/Adafruit_Python_DHT/examples/AdafruitDHT.py 2302 4 >> /dev/null &
+#watch -n 5 python3 /home/pi/gromari/raspi/coretemp/coretemp.py >> /dev/null &
+#watch -n 5 sudo /home/pi/sources/Adafruit_Python_DHT/examples/AdafruitDHT.py 2302 4 >> /dev/null &
 cd /home/pi/gromari/raspi/
 docker-compose -f docker-compose.yml up --force-recreate
