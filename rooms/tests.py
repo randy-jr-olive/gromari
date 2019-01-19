be81725a5a6ac39174fdd2773b3720d6a738b438
Randy Olive <randy.jr.olive@gmail.com>
2019-01-19 18:30:27 -0500

    Resolve "add unit tests to dev and gitlab CI builds"

--- rooms/tests.py
+++ rooms/tests.py
@@ -1,3 +1,34 @@
-from django.test import TestCase
+from django.test import Client, TestCase
+from django.urls import reverse, resolve
+from rooms.views import rooms
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class RoomTests(TestCase):
+
+    client = Client()
+
+    def setUp(self):
+        # creates a test user to check login/logout functionality
+        self.user = User.objects.create_user(
+            username='testuser', password='12345')
+
+    def testLoginRequired(self):
+        # tests to make sure that the user must login first and will be redirected
+        # to the login page from any URL they try to access without login
+
+        response = self.client.get(reverse('rooms'))
+        self.assertEqual(response.status_code, 302)
+
+    def testLogin(self):
+        # tests to make sure that the user can login and access the rooms page
+
+        login = self.client.login(username='testuser', password='12345')
+        response = self.client.get(reverse('rooms'))
+        self.assertEqual(response.status_code, 200)
+
+    def testRoomsURLFunction(self):
+        # checks if the URL 'rooms' reolves the correct rooms function
+
+        found = resolve(reverse('rooms'))
+        self.assertEqual(found.func, rooms)
