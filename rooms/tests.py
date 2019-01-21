1d319ec33e13d39abac0437e1ca3806446f08948
Randy Olive <randy.jr.olive@gmail.com>
2019-01-21 11:50:25 -0500

    added test for tasks.py

--- rooms/tests.py
+++ rooms/tests.py
@@ -2,6 +2,10 @@ from django.test import Client, TestCase
 from django.urls import reverse, resolve
 from rooms.views import rooms
 from django.contrib.auth.models import User
+from rooms.tasks import createEnviroReadingTask
+from rooms.models import SensorData, Room
+import unittest
+import pytest
 
 
 class RoomTests(TestCase):
@@ -38,3 +42,21 @@ class RoomTests(TestCase):
         login = self.client.login(username='testuser', password='12345')
         response = self.client.get(reverse('rooms'))
         self.assertContains(response, "<title>gromari - Rooms</title>")
+
+
+class TaskTests(unittest.TestCase):
+
+    def setUp(self):
+        Room.objects.create(name="testing", description="testing room",
+                            lightsOn=1, lightsOff=2)
+
+    @pytest.mark.django_db(transaction=True)
+    def testCreateEnviroReadingTask(self):
+        testRoom = Room.objects.get(name="testing")
+
+        createEnviroReadingTask(testRoom.id)
+
+        latestReading = SensorData.objects.filter(
+            room_fk=testRoom.id).latest('timestamp')
+
+        self.assertIsInstance(latestReading, SensorData)
