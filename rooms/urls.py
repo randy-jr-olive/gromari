cbdd55323c32d0267f750957e67bbe2b12477187
Randy Olive <randy.jr.olive@gmail.com>
2019-02-07 00:13:26 -0500

    Resolve "create basic journal entry system"

--- rooms/urls.py
+++ rooms/urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
     path('room/delete/<int:room_id>', views.deleteRoom, name='deleteRoom'),
     path('room/<int:room_id>/plant/<int:plant_id>', views.plantDetails, name='plantDetails'),
     path('plant/delete/<int:plant_id>', views.deletePlant, name='deletePlant'),
+    path('journal', views.journalHome, name='journalHome'),
+    path('journal/delete/<int:journal_id>', views.deleteJournalEntry, name='deleteJournalEntry'),
+    path('journal/<int:journal_id>', views.journalDetails, name='journalDetails'),
+    path('journal/<int:journal_id>/<str:room_tag>', views.journalDetails, name='journalDetails'),
 ]
